c8650bb78e2688664623dfe68139ccc30a7d8ccb
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-19 17:39:11 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,19 @@
+class Doctor
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+
+end
